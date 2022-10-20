ed343879c46ca964d840324c3c75e9c4751c0496
Tr_jik <tsheporamabetha@gmail.com>
2022-10-20 18:17:48 +0200

    Added functions file

--- _printf.c
+++ _printf.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -51,11 +48,8 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
